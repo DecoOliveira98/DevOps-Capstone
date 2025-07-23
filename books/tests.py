bd4dee6c5386ef7c1f0350b250327f8f720ffc4e
Andre Oliveira <aloliveira98@outlook.com>
2025-07-23 17:46:32 +0100

    [feat] runned a test.py and answer was 'OK'

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Book
 
-# Create your tests here.
+class BookAPITests(APITestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Author Example",
+            isbn="1234567890",
+            published_date="2022-01-01"
+        )
+
+    def test_list_books(self):
+        url = reverse('book-list')  # URL for listing books
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('Test Book', response.content.decode())
+
+    def test_create_book(self):
+        url = reverse('book-list')
+        data = {
+            "title": "Another Book",
+            "author": "Another Author",
+            "isbn": "0987654321",
+            "published_date": "2022-02-02"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+
+    def test_delete_book(self):
+        url = reverse('book-detail', kwargs={'pk': self.book.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
